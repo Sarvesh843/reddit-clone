beccaca99fec4405a46ae56c8f57254cc5012292
Sarvesh843 <sarveshsyadav8@gmail>
2023-09-18 13:16:05 +0530

    first

---
+++ src/components/Layout/PageContent.tsx
@@ -0,0 +1,35 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+type PageContentProps = {};
+
+const PageContent: React.FC<PageContentProps> = ({ children }) => {
+  return (
+    <Flex justify="center" p="16px 0px" >
+      <Flex
+        width="95%"
+        justify="center"
+        maxWidth="860px"
+        
+      >
+        <Flex
+          direction="column"
+          width={{ base: "100%", md: "65%" }}
+          mr={{ base: 0, md: 6 }}
+          
+        >
+          {children[0]}
+        </Flex>
+
+        <Flex
+          direction="column"
+          display={{ base: "none", md: "flex" }}
+          flexGrow={1}
+        >
+          {children[1]}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+export default PageContent;
