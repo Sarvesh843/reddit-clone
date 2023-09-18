60f2c132077c71440e6a79b1eb6b33620565f030
Sarvesh843 <sarveshsyadav8@gmail>
2023-09-18 13:51:03 +0530

    third

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -7,19 +7,23 @@ type SearchInputProps = {
   user?: User | null;
 };
 
-const SearchInput: React.FC<SearchInputProps> = ({user}) => {
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
   return (
-    <Flex flexGrow={1} maxWidth={user? "auto" : "600px"} mr={2} align="center">
+    <Flex flexGrow={1} maxWidth={user ? "auto" : "600px"} mr={2} align="center">
       <InputGroup>
         <InputLeftElement pointerEvents="none">
-          <SearchIcon color="gray.300" mb={1}/>
+          <SearchIcon color="gray.300" mb={1} />
         </InputLeftElement>
         <Input
           placeholder="Search Reddit"
           fontSize="10pt"
           _placeholder={{ color: "gray.500" }}
           _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
-          _focus={{outline:"none",border:"1px solid", borderColor: "blue.500" }}
+          _focus={{
+            outline: "none",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
           height="34px"
           bg="gray.50"
         />
--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -21,17 +21,18 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
 
   const setCommunityStateValue = useSetRecoilState(communityState);
 
-  if (!communityData) {
-    return <NotFound />;
-  }
-
+  
   useEffect(() => {
     setCommunityStateValue((prev) => ({
       ...prev,
       currentCommunity: communityData,
     }));
   }, [communityData]);
-
+  
+  if (!communityData) {
+    return <NotFound />;
+  }
+  
   return (
     <>
       <Header communityData={communityData} />
