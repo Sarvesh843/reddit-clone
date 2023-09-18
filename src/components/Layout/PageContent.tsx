873a7f810226c7050c59b74f88a87f48e63a5964
Sarvesh843 <sarveshsyadav8@gmail>
2023-09-18 13:57:51 +0530

    build-fix

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -1,22 +1,18 @@
 import { Flex } from "@chakra-ui/react";
 import React from "react";
 
-type PageContentProps = {};
+type PageContentProps = {
+  children: any;
+};
 
 const PageContent: React.FC<PageContentProps> = ({ children }) => {
   return (
-    <Flex justify="center" p="16px 0px" >
-      <Flex
-        width="95%"
-        justify="center"
-        maxWidth="860px"
-        
-      >
+    <Flex justify="center" p="16px 0px">
+      <Flex width="95%" justify="center" maxWidth="860px">
         <Flex
           direction="column"
           width={{ base: "100%", md: "65%" }}
           mr={{ base: 0, md: 6 }}
-          
         >
           {children[0]}
         </Flex>
